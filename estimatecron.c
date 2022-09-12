68eeeca9a8d278506016243c2cca0f02a3ee3ca8
hurrkage <100751647+hurrkage@users.noreply.github.com>
2022-09-12 18:05:38 +0800

    Quick updates

--- estimatecron.c
+++ estimatecron.c
@@ -1,7 +1,12 @@
+// Cits2002 Project 1 2022
+//  Student1:   23102622   Cura   Jericho
+//  Student2:   Chin   Chin  Aaron
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
 int main()
 {
 	FILE* fPointer;
